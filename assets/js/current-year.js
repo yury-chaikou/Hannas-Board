73fde51d319bdeb5ea23003ad7bb77d511e7752b
yury-chaikou <68887857+yury-chaikou@users.noreply.github.com>
2020-08-03 14:30:57 +0300

    Update current-year.js

--- assets/js/current-year.js
+++ assets/js/current-year.js
@@ -3,5 +3,5 @@ $(document).ready(function () {
     if (year ===2020){
         $("#current_year").append('2020 - ');
     }else {
-    jQuery("#current_year").append('2020 - '+  year);}
+    $("#current_year").append('2020 - '+  year);}
 });
